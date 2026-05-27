b35a41904c386187ed69efd9bc7fa8176c0a2064
mishael6 <mishosmanmensah660@gmail.com>
2026-05-27 23:58:29 +0000

    fix express 5 options route

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));
 
-app.options('*', cors());
+app.options('/{*path}', cors());
 
 const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
